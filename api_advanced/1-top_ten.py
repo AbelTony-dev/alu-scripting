8976d27e7d4b9493b1ad8a2ae76df8d222d7fd86
Abel Tony <122355309+AbelTony-dev@users.noreply.github.com>
2023-08-27 18:34:53 +0200

    Add files via upload

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""top ten"""
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """a function that prints the titles of the first 10 hot posts"""
-    headers = {'User-Agent': 'My API advanced 1.0'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-    else:
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
